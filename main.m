d6a57905c70955a41048796b7f80bd2352de0c39
X. Zhou <P318609@workspace.rug.nl>
2024-11-04 18:02:36 +0100

    main function

---
+++ main.m
@@ -0,0 +1,20 @@
+% You only need to run this main function
+function main
+    n = 20; % total number of agents
+    
+    female_rate = 0.5;
+
+    K = 10; % total lnumber of items need to rate
+    k = 5; % the number of the most importnat items that need to ensured
+
+    alpha = 0.1; % increament / decreament unit of claiming tendecy
+    beta = 0.1; % increament / decreament unit of granting tendecy
+    % female tendency + noise
+    % male tendency + noise
+
+    p = 0.5; % influence radius
+    T = 50; % termination time step
+    
+    
+
+end
